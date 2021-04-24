d13f676b37476b729ab7bde7c9d02795a0cce29e
Pager Chan <0xtaipoian@protonmail.com>
2021-04-24 19:23:39 +0800

    add title to the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
+import { Head } from 'next/document';
 import React, { useContext, useEffect, useState } from 'react';
 import { Box, Button, Card, Heading, Image, Text } from 'rebass/styled-components';
 import styled from 'styled-components';
@@ -46,6 +47,9 @@ const IndexPage: NextPage = () => {
 
   return (
     <Page>
+      <Head>
+        <title>NFT PoC</title>
+      </Head>
       <Navigation />
       {error ? (
         <Text color="red" p={3} textAlign="center">
